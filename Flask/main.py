eade491fbbcfdae620476761824024c7a028bdef
thisishwan2 <feel000617@gmail.com>
2024-05-01 17:21:16 +0900

    feat: user info submit api 생성

--- Flask/main.py
+++ Flask/main.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from urllib import request
+
+from flask import Flask, render_template, session, redirect, url_for
+
 app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    session['name'] = request.form.get('name')
+    session['student_number'] = request.form.get('student_number')
+    session['gender'] = request.form.get('gender')
+    session['major'] = request.form.get('major')
+    session['programming_languages'] = request.form.getlist('programming_language')
+
+    return redirect(url_for('result'))
 
 
 if __name__ == '__main__':
